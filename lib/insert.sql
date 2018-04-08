5ca13a7ad5b11b212d064dd6a3f25d151dfb4868
briecodes <hello@brie.work>
2018-04-08 18:36:48 -0400

    SQL queries defined.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 25, "M", "Brown", "Angry", "True"),
+(2, "Rowdy", 5, "F", "Black", "Calm", "True"),
+(3, "Tabitha", 9, "M", "White", "Nervous", "False"),
+(4, "Sergeant Brown", 55, "M", "Brown", "Tired", "True"),
+(5, "Melissa", 22, "F", "Black", "Snobbish", "True"),
+(6, "Grinch", 43, "M", "Grey", "Grumpy", "True"),
+(7, "Wendy", 88, "F", "Brown", "Kind", "True"),
+(8, NULL, 18, "M", "Brown", "Regretful", "False");
